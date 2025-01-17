f3743dcab5ba98404982de467a73e2e1ad07a73a
Joe <ckdfo1995@gmail.com>
2025-01-17 17:52:54 +0900

    add sse function on Server

--- public/javascripts/sse.js
+++ public/javascripts/sse.js
@@ -1,32 +1,42 @@
 // server-sent events 테스트용 코드
-const SSE_URL = "123.html";
-const eventSrc = new EventSource(SSE_URL);
+const SSE_URL = "/users/sseDemo";
+let eventSrc = null;
 
-console.log(eventSrc);
-
-eventSrc.onmessage = function (e) {
-    const newElement = document.createElement("li");
-    const eventList = document.getElementById("list");
-
-    newElement.textContent = `msg: ${e.data}`;
-    eventList.appendChild(newElement);
-};
-
-eventSrc.addEventListener("ping", (e) => {
-    const newElement = document.createElement("li");
-    const eventList = document.getElementById("list");
-
-    const time = JSON.parse(e.data).time;
-    newElement.textContent = `ping at ${time}`;
-    eventList.appendChild(newElement);
-});
-
-eventSrc.onerror = (e) => {
-    alert("eventSource faild to.");
-};
-
+// button event
 const stopBtn = document.querySelector(".stopBtn");
+const startBtn = document.querySelector(".startBtn");
+
 stopBtn?.addEventListener("click", (e) => {
-    console.log("sse closed");
-    eventSrc.close();
+    eventSrc?.close();
+    // delete eventSrc;
+    console.log("closeBtn:", eventSrc);
 });
+
+startBtn?.addEventListener("click", (e) => {
+    eventSrc = new EventSource(SSE_URL);
+    setEventSrc(eventSrc);
+});
+
+function setEventSrc(_eventSrc) {
+    _eventSrc?.onmessage = function (e) {
+        const newElement = document.createElement("li");
+        const eventList = document.getElementById("list");
+
+        newElement.textContent = `msg: ${e.data}`;
+        eventList.appendChild(newElement);
+    };
+
+    _eventSrc?.addEventListener("ping", (e) => {
+        const newElement = document.createElement("li");
+        const eventList = document.getElementById("list");
+
+        const time = JSON.parse(e.data).time;
+        newElement.textContent = `ping at ${time}`;
+        eventList.appendChild(newElement);
+    });
+
+    _eventSrc?.onerror = (e) => {
+        alert("eventSource faild to.");
+        eventSrc?.close();
+    };
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    res.send("respond with a resource");
+});
+
+router.get("/sseDemo", function (req, res, next) {
+    res.setHeader("X-Accel-Buffering", "no");
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+
+    const timeId = setInterval(() => {
+        console.log("setInterval on sseDemo");
+        const data = {data: "dummy data!"};
+        res.send(JSON.stringify(data));
+    }, 3000);
 });
 
 module.exports = router;
